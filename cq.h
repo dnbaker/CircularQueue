4417e7dc78d412a4f96a842ff3701f040c7bb767
dnbaker <dnb@cs.jhu.edu>
2019-10-04 11:47:12 -0400

    Be a good citizen and use iterator traits. This allows us to sort the container as well as rotate.

--- cq.h
+++ cq.h
@@ -11,6 +11,8 @@
 #include <cstring>     // For std::memcpy
 #include <vector>      // For converting to vector
 #include <climits>     // CHAR_BIT
+#include <iostream>
+#include <algorithm>
 
 namespace circ {
 using std::size_t;
@@ -25,6 +27,10 @@ using std::uint8_t;
 #define CIRC_CONSTIF if
 #endif
 
+template<typename T, typename SizeType=uint32_t>
+class deque;
+
+
 template<typename T>
 static inline T roundup(T x) {
     // With -O2 or higher, this full unrolls for all types
@@ -35,7 +41,165 @@ static inline T roundup(T x) {
     return ++x;
 }
 
-template<typename T, typename SizeType=uint32_t>
+template<typename T, typename SizeType>
+class circular_iterator {
+    using size_type = SizeType;
+    // TODO: increment by an integral quantity.
+    using deque_type = deque<T, SizeType>;
+    deque_type *ref_;
+    deque_type &ref() {return *ref_;}
+    const deque_type &ref() const {return *ref_;}
+    SizeType           pos_;
+public:
+    circular_iterator(deque_type &reff, SizeType pos): ref_(std::addressof(reff)), pos_(pos) {}
+    circular_iterator(const circular_iterator &other): ref_(other.ref_), pos_(other.pos_) {}
+    std::ptrdiff_t operator-(circular_iterator o) const {
+        return (o.pos_ - pos_) & ref().mask();
+    }
+    std::ptrdiff_t operator+(circular_iterator o) const {
+        return (o.pos_ + pos_) & ref().mask();
+    }
+    circular_iterator &operator=(const circular_iterator &o) {
+        this->ref_ = o.ref_;
+        this->pos_ = o.pos_;
+        return *this;
+    }
+    circular_iterator operator-(std::ptrdiff_t i) const {
+        circular_iterator tmp(*this);
+        tmp -= i;
+        return tmp;
+    }
+    circular_iterator operator+(std::ptrdiff_t i) const {
+        circular_iterator tmp(*this);
+        tmp += i;
+        return tmp;
+    }
+    circular_iterator &operator-=(std::ptrdiff_t i) {
+        pos_ -= i;
+        pos_ &= ref().mask();
+        return *this;
+    }
+    circular_iterator &operator+=(std::ptrdiff_t i) {
+        pos_ += i;
+        pos_ &= ref().mask();
+        return *this;
+    }
+    T &operator*() {
+        return ref().data()[pos_];
+    }
+    const T &operator*() const noexcept {
+        return ref().data()[pos_];
+    }
+    T *operator->() {
+        return &ref().data()[pos_];
+    }
+    const T *operator->() const {
+        return &ref().data()[pos_];
+    }
+    circular_iterator &operator++() noexcept {
+        ++pos_;
+        pos_ &= ref().mask();
+        return *this;
+    }
+    circular_iterator operator++(int) noexcept {
+        circular_iterator copy(*this);
+        this->operator++();
+        return copy;
+    }
+    bool operator==(const circular_iterator &other) const noexcept {
+        return pos_ == other.pos_;
+    }
+    bool operator!=(const circular_iterator &other) const noexcept {
+        return pos_ != other.pos_;
+    }
+    bool operator<(const circular_iterator &other) const noexcept {
+        return pos_ < other.pos_;
+    }
+    bool operator<=(const circular_iterator &other) const noexcept {
+        return pos_ <= other.pos_;
+    }
+    bool operator>(const circular_iterator &other) const noexcept {
+        return pos_ > other.pos_;
+    }
+    bool operator>=(const circular_iterator &other) const noexcept {
+        return pos_ >= other.pos_;
+    }
+};
+template<typename T, typename SizeType>
+class const_circular_iterator {
+    using size_type = SizeType;
+    using deque_type = deque<T, SizeType>;
+    const deque_type *ref_;
+    SizeType          pos_;
+    auto &ref() {return *ref_;}
+public:
+    const_circular_iterator(const deque_type &ref, SizeType pos): ref_(&ref), pos_(pos) {}
+    const_circular_iterator(const const_circular_iterator &other): ref_(&other.ref_), pos_(other.pos_) {}
+    std::ptrdiff_t operator-(const_circular_iterator o) const {
+        return (o.pos_ - pos_) & ref().mask();
+    }
+    std::ptrdiff_t operator+(const_circular_iterator o) const {
+        return (o.pos_ + pos_) & ref().mask();
+    }
+    const_circular_iterator &operator=(const const_circular_iterator &o) {
+        this->ref_ = o.ref_;
+        this->pos_ = o.pos_;
+        return *this;
+    }
+    const_circular_iterator operator-(std::ptrdiff_t i) const {
+        const_circular_iterator tmp(*this);
+        tmp -= i;
+        return tmp;
+    }
+    const_circular_iterator operator+(std::ptrdiff_t i) const {
+        const_circular_iterator tmp(*this);
+        tmp += i;
+        return tmp;
+    }
+    const_circular_iterator &operator-=(std::ptrdiff_t i) {
+        pos_ -= i;
+        pos_ &= ref().mask();
+        return *this;
+    }
+    const_circular_iterator &operator+=(std::ptrdiff_t i) {
+        pos_ += i;
+        pos_ &= ref().mask();
+        return *this;
+    }
+    const T &operator*() const noexcept {
+        return ref().data()[pos_];
+    }
+    const_circular_iterator &operator++() noexcept {
+        ++pos_;
+        pos_ &= ref().mask();
+        return *this;
+    }
+    const_circular_iterator operator++(int) noexcept {
+        const_circular_iterator copy(*this);
+        this->operator++();
+        return copy;
+    }
+    bool operator==(const const_circular_iterator &other) const noexcept {
+        return pos_ == other.pos_;
+    }
+    bool operator!=(const const_circular_iterator &other) const noexcept {
+        return pos_ != other.pos_;
+    }
+    bool operator<(const const_circular_iterator &other) const noexcept {
+        return pos_ < other.pos_;
+    }
+    bool operator<=(const const_circular_iterator &other) const noexcept {
+        return pos_ <= other.pos_;
+    }
+    bool operator>(const const_circular_iterator &other) const noexcept {
+        return pos_ > other.pos_;
+    }
+    bool operator>=(const const_circular_iterator &other) const noexcept {
+        return pos_ >= other.pos_;
+    }
+};
+
+template<typename T, typename SizeType>
 class deque {
     // A circular queue in which extra memory has been allocated up to a power of two.
     // This allows us to use bitmasks instead of modulus operations.
@@ -49,104 +213,8 @@ class deque {
 
 public:
     using size_type = SizeType;
-    class iterator {
-        // TODO: increment by an integral quantity.
-        deque &ref_;
-        SizeType           pos_;
-    public:
-        iterator(deque &ref, SizeType pos): ref_(ref), pos_(pos) {}
-        iterator(const iterator &other): ref_(other.ref_), pos_(other.pos_) {}
-        T &operator*() {
-            return ref_.data_[pos_];
-        }
-        const T &operator*() const noexcept {
-            return ref_.data_[pos_];
-        }
-        T *operator->() {
-            return &ref_.data_[pos_];
-        }
-        const T *operator->() const {
-            return &ref_.data_[pos_];
-        }
-        iterator &operator++() noexcept {
-            ++pos_;
-            pos_ &= ref_.mask_;
-            return *this;
-        }
-        iterator operator++(int) noexcept {
-            iterator copy(*this);
-            this->operator++();
-            return copy;
-        }
-        bool operator==(const iterator &other) const noexcept {
-            return pos_ == other.pos_;
-        }
-        bool operator!=(const iterator &other) const noexcept {
-            return pos_ != other.pos_;
-        }
-        bool operator<(const iterator &other) const noexcept {
-            return pos_ < other.pos_;
-        }
-        bool operator<=(const iterator &other) const noexcept {
-            return pos_ <= other.pos_;
-        }
-        bool operator>(const iterator &other) const noexcept {
-            return pos_ > other.pos_;
-        }
-        bool operator>=(const iterator &other) const noexcept {
-            return pos_ >= other.pos_;
-        }
-    };
-    class const_iterator {
-        const deque &ref_;
-        SizeType                 pos_;
-    public:
-        const_iterator(const deque &ref, SizeType pos): ref_(ref), pos_(pos) {}
-        const_iterator(const const_iterator &other): ref_(other.ref_), pos_(other.pos_) {}
-        const T &operator*() const noexcept {
-            return ref_.data_[pos_];
-        }
-        const_iterator &operator++() noexcept {
-            ++pos_;
-            pos_ &= ref_.mask_;
-            return *this;
-        }
-        const_iterator operator++(int) noexcept {
-            const_iterator copy(*this);
-            this->operator++();
-            return copy;
-        }
-        bool operator==(const const_iterator &other) const noexcept {
-            return pos_ == other.pos_;
-        }
-        bool operator!=(const const_iterator &other) const noexcept {
-            return pos_ != other.pos_;
-        }
-        bool operator<(const const_iterator &other) const noexcept {
-            return pos_ < other.pos_;
-        }
-        bool operator<=(const const_iterator &other) const noexcept {
-            return pos_ <= other.pos_;
-        }
-        bool operator>(const const_iterator &other) const noexcept {
-            return pos_ > other.pos_;
-        }
-        bool operator>=(const const_iterator &other) const noexcept {
-            return pos_ >= other.pos_;
-        }
-    };
-    iterator begin() noexcept {
-        return iterator(*this, start_);
-    }
-    iterator end() noexcept {
-        return iterator(*this, stop_);
-    }
-    const_iterator cbegin() const noexcept {
-        return const_iterator(*this, start_);
-    }
-    const_iterator cend()   const noexcept {
-        return const_iterator(*this, stop_);
-    }
+    using iterator = circular_iterator<T, size_type>;
+    using const_iterator = const_circular_iterator<T, size_type>;
     deque(SizeType size):
             mask_(roundup(size + 1) - 1),
             start_(0), stop_(0),
@@ -172,11 +240,23 @@ public:
         data_ = tmp;
         for(auto i(other.start_); i != other.stop_; data_[i] = other.data_[i], i = (i+1) & mask_);
     }
+    iterator begin() noexcept {
+        return iterator(*this, start_);
+    }
+    iterator end() noexcept {
+        return iterator(*this, stop_);
+    }
+    const_iterator cbegin() const noexcept {
+        return const_iterator(*this, start_);
+    }
+    const_iterator cend()   const noexcept {
+        return const_iterator(*this, stop_);
+    }
     auto start() const {return start_;}
     auto stop() const {return stop_;}
     auto mask() const {return mask_;}
     auto data() const {return data_;}
-    auto data()             {return data_;}
+    auto data()       {return data_;}
     void resize(size_type new_size) {
         // TODO: this will cause a leak if the later malloc fails.
         // Fix this by only copying the temporary buffer to data_ in case of success in all allocations.
@@ -187,6 +267,7 @@ public:
         if(tmp == nullptr) throw std::bad_alloc();
         data_ = static_cast<T *>(tmp);
         if(start_ == stop_) {
+            std::fprintf(stderr, "stop == start\n");
             if(start_) {
                 stop_ = mask_ + 1;
                 auto tmp = static_cast<T *>(std::malloc((stop_) * sizeof(T)));
@@ -198,6 +279,12 @@ public:
                 start_ = 0;
             }
         } else if(stop_ < start_) {
+            std::fprintf(stderr, "stop < start\n");
+            auto shift = start_;
+            std::fprintf(stderr, "size before: %zu.\n", size());
+            auto tmp = size();
+            std::rotate(this->begin(), this->end(), iterator(*this, mask_));
+#if 0
             auto tmp = static_cast<T *>(std::malloc((mask_ + 1) * sizeof(T)));
             if(tmp == nullptr) throw std::bad_alloc();
             std::memcpy(tmp, data_ + start_, ((mask_ + 1) - start_) * sizeof(T));
@@ -205,8 +292,12 @@ public:
             auto sz = (stop_ - start_) & mask_;
             std::memcpy(data_, tmp, sz);
             std::free(tmp);
+#endif
             start_ = 0;
-            stop_ = sz;
+            stop_ = tmp;
+            std::fprintf(stderr, "size after: %zu.\n", size());
+        } else {
+            std::fprintf(stderr, "stop > start\n");
         }
         mask_ = new_size - 1;
     }
@@ -224,11 +315,25 @@ public:
         return *(new(data_ + ind) T(std::forward<Args>(args)...));
     }
     template<typename... Args>
-    T &emplace_back(Args &&... args) {
+    T &push_front(Args &&... args) {
+        if(((start_ - 1) & mask_) == stop_) {
 #if !NDEBUG
-        //std::fprintf(stderr, "Emplacing back %s\n", __PRETTY_FUNCTION__);
+            std::fprintf(stderr, "Resizing to new size of %zu\n", (size_t(mask_ + 1) << 1));
 #endif
+            resize((mask_ + 1) << 1);
+        }
+        start_ = (start_ - 1) & mask_;
+        assert(start_ <= mask_);
+        assert(start_ > 0);
+        return *(new(data_ + start_) T(std::forward<Args>(args)...));
+    }
+    template<typename... Args>
+    T &emplace_back(Args &&... args) {
         return push_back(std::forward<Args>(args)...); // Interface compatibility.
+    }
+    template<typename... Args>
+    T &emplace_front(Args &&... args) {
+        return push_front(std::forward<Args>(args)...); // Interface compatibility.
     }
     T pop() {
         if(__builtin_expect(stop_ == start_, 0)) throw std::runtime_error("Popping item from empty buffer. Abort!");
@@ -271,6 +376,14 @@ public:
     void for_each(const Functor &func) {
         for(SizeType i = start_; i != stop_; func(data_[i++]), i &= mask_);
     }
+    template<typename Functor>
+    void for_each(const Functor &func) const {
+        for(SizeType i = start_; i != stop_; func(data_[i++]), i &= mask_);
+    }
+    void show() const {
+        assert(data_);
+        for_each([](auto x) {std::cerr << x << '\n';});
+    }
     ~deque() {this->free();}
     size_type capacity() const noexcept {return mask_;}
     size_type size()     const noexcept {return (stop_ - start_) & mask_;}
@@ -281,7 +394,7 @@ public:
         return ret;
     }
     void clear() {
-        CIRC_CONSTIF(std::is_destructible<T>::value)
+        //CIRC_CONSTIF(std::is_destructible<T>::value)
             for(size_type i(start_); i != stop_; data_[i++].~T(), i &= mask_);
         start_ = stop_ = 0;
     }
@@ -292,4 +405,24 @@ public:
 }; // deque
 
 } // namespace circ
+namespace std {
+template<typename T, typename SizeType>
+struct iterator_traits<circ::circular_iterator<T, SizeType>> {
+    using difference_type = std::ptrdiff_t;
+    using reference_type = T &;
+    using pointer        = T *;
+    using value_type = T;
+    struct iterator_category: public forward_iterator_tag {};
+};
+
+template<typename T, typename SizeType>
+struct iterator_traits<circ::const_circular_iterator<T, SizeType>> {
+    using difference_type = std::ptrdiff_t;
+    using reference_type = T &;
+    using pointer        = T *;
+    using value_type = T;
+    struct iterator_category: public forward_iterator_tag {};
+};
+} // namespace std
+
 #endif /* #ifndef CIRCULAR_QUEUE_H__ */
